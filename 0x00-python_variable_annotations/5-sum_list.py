bfe1a193761b0ce1e83be4a4b1b59458d9f46b83
Eric Ogbekene <ericogbekene@icloud.com>
2024-06-06 14:37:18 +0100

    function to sum a list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+module to Sum List
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    function to sum float elememts of a list
+    """
+    return sum(input_list)
