80026e1d994e1229c8344150bf6f8d0dd2286efd
Eric Ogbekene <ericogbekene@icloud.com>
2024-06-07 00:25:23 +0100

    creating a custom tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,8 +4,11 @@ module to return a tuple
 """
 from typing import Tuple, Union
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     function to return a custom tuple
     """
-    return tuple((k, float(v ** 2)))
+    try:
+        return tuple((k, float(v) ** 2))
+    except ValueError:
+        raise TypeError("Value must be convertible to float.")
