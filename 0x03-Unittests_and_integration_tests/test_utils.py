156d87ddf9a7da0ff46ea739014970f66d6233b5
Eric Ogbekene <ericogbekene@icloud.com>
2024-06-30 18:49:50 +0100

    assert that it raises a particular error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,3 +23,14 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path)
+        """
+        test that the method raises an Error
+        """
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path)
+
